ffd997a86b975fb6c919e7d2aaa51bc23c4110c6
r21347 <r21347@K2212019.rsoa.ryobi.co.jp>
2023-07-28 02:03:11 +0900

    トップページアクションの作成

--- src/main/java/services/ReportService.java
+++ src/main/java/services/ReportService.java
@@ -38,7 +38,7 @@ public class ReportService extends ServiceBase {
          * @param employee
          * @return 日報データの件数
          */
-    public long countAllmine(EmployeeView employee) {
+    public long countAllMine(EmployeeView employee) {
 
         long count = (long) em.createNamedQuery(JpaConst.Q_REP_COUNT_ALL_MINE, Long.class)
                 .setParameter(JpaConst.JPQL_PARM_EMPLOYEE, EmployeeConverter.toModel(employee))
